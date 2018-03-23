17eb64b253cd31d447bff86cb00fc03bffba1cf8
Carlos R. Arias <arias@spu.edu>
2018-03-22 17:20:28 -0700

    Adding some js 002

---
+++ code.js
@@ -0,0 +1,7 @@
+function calculateTotal(){
+	var total = 0;
+	var a = document.getElementById("a").value;
+	var b = document.getElementById("b").value;
+	document.getElementById("result").value = a + b;
+
+}
